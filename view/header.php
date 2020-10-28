b3f9305c053c5e7ba1abb72aa7091e7fde2d989b
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-10-28 15:13:18 +0100

    Update header

--- view/header.php
+++ view/header.php
@@ -1,11 +1,13 @@
 <ul>
-    <section class="header-left">
+    <div class="header-left">
         <li><a class="active" href="/">M.BURNAT</a></li>
-        <li><a href="/info-dev">Informatique - Développement</a></li>
-        <li><a href="#contact">Informatique - Création Techniques </a></li>
-        <li><a href="#contact">Marketing </a></li>
-        <li><a href="#about">Divers</a></li>
-        <li><a href="#about"> Rente-Me/Contact </a></li>
-    </section>
+    </div>
 
+    <div class="header-right">
+        <li><a href="/info-dev">Informatique - Développement</a></li>
+        <li><a href="/info-crea-tech">Informatique - Création Techniques </a></li>
+        <li><a href="/marketing">Marketing </a></li>
+        <li><a href="/divers">Divers</a></li>
+        <li><a href="/contact"> Rente-Me/Contact </a></li>
+    </div>
 </ul>
