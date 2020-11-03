26e6b936c4ec486fbffe6d6ba0f06575dd755431
Math <math.oron@gmail.com>
2020-11-03 17:35:30 +0100

    Adding fonts and improve the header

--- view/homepage.php
+++ view/homepage.php
@@ -3,16 +3,13 @@
 ?>
 
 <container class="hp-container">
-    <div class="hp-text">
-        <h1>
-            Mathieu Burnat
-        </h1>
+    <h1>
+        Mathieu Burnat
+    </h1>
 
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad ullam soluta repellendus ab saepe doloribus? Similique laborum minus molestiae quod, error porro sint iure, consequuntur et qui at ullam.
-        <p>
-    </div>
-
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad ullam soluta repellendus ab saepe doloribus? Similique laborum minus molestiae quod, error porro sint iure, consequuntur et qui at ullam.
+    <p>
     <div class="hp-button"> *button* </div>
 </container>
 
