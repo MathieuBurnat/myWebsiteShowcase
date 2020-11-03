378773a677823dd948c67e92345bccfbf61dfb91
Math <math.oron@gmail.com>
2020-11-03 18:03:55 +0100

    Adding homepage

--- view/homepage.php
+++ view/homepage.php
@@ -2,14 +2,16 @@
     require('view/header.php');
 ?>
 
-<container class="hp-container">
-    <h1>
-        Mathieu Burnat
-    </h1>
+<div class="hp-container">
+    <div class="hp-text">
+        <h1>
+            Mathieu Burnat
+        </h1>
 
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad ullam soluta repellendus ab saepe doloribus? Similique laborum minus molestiae quod, error porro sint iure, consequuntur et qui at ullam.
-    <p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad ullam soluta repellendus ab saepe doloribus? Similique laborum minus molestiae quod, error porro sint iure, consequuntur et qui at ullam.
+        <p>
+    </div>
     <div class="hp-button"> *button* </div>
-</container>
+</div>
 
