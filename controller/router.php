1c9ce39d2b792014f3b953f37f9d7e872781e7fa
Math <math.oron@gmail.com>
2020-11-04 14:29:26 +0100

    Add footer

--- controller/router.php
+++ controller/router.php
@@ -16,38 +16,32 @@
         case '/dev-header' :
             $path = "view/header.php";
             break;
-
+        case '/dev-footer' :
+            $path = "view/footer.php";
+            break;
         //Normal request
         case '' :
         case '/' :
             $path = "view/homepage.php";
             break;
-
         case '/bio' :
             $path = "view/bio.php";
             break;
-
         case '/info-dev' :
             $path = "view/info-dev.php";
-
             break;
-
         case '/info-crea-tech' :
             $path = "view/info-crea-tech.php";
             break;    
-
         case '/marketing' :
             $path = "view/marketing.php";
             break;    
-
         case '/divers' :
             $path = "view/divers.php";
             break;
-
         case '/contact' :
             $path = "view/contact.php";
             break; 
-
         default:
             http_response_code(404);
             $path = "view/Error_404.php"; //Aie, something wrong ! (page not found)
--- view/bio.php
+++ view/bio.php
@@ -1,4 +1,9 @@
 <?php
     require('view/header.php');
 ?>
+
 <h1> Mathieu's bio </h1>
+
+<?php
+    require('view/footer.php');
+?>
