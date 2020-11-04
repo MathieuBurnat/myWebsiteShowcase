7c23426685882bbe2f78677c832df831cace75ef
Math <math.oron@gmail.com>
2020-11-04 09:20:35 +0100

    Remove the scrollbar

--- view/homepage.php
+++ view/homepage.php
@@ -2,16 +2,18 @@
     require('view/header.php');
 ?>
 
-<div class="hp-container">
-    <div class="hp-text">
-        <h1>
-            Mathieu Burnat
-        </h1>
+<body class="hp-body">
+    <div class="hp-container">
+        <div class="hp-text">   
+            <h1>
+                Mathieu Burnat
+            </h1>
 
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad ullam soluta repellendus ab saepe doloribus? Similique laborum minus molestiae quod, error porro sint iure, consequuntur et qui at ullam.
-        <p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad ullam soluta repellendus ab saepe doloribus? Similique laborum minus molestiae quod, error porro sint iure, consequuntur et qui at ullam.
+            <p>
+        </div>
+        <div class="hp-button"> *button* </div>
     </div>
-    <div class="hp-button"> *button* </div>
-</div>
+</body>
 
