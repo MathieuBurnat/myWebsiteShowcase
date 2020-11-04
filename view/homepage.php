bb5a50431eea8bc5768418d4afcb1e87f36fdff4
Math <math.oron@gmail.com>
2020-11-04 14:02:33 +0100

    Update hp-button's style

--- view/homepage.php
+++ view/homepage.php
@@ -10,7 +10,7 @@
             </h1>
 
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad ullam soluta repellendus ab saepe doloribus? Similique laborum minus molestiae quod, error porro sint iure, consequuntur et qui at ullam.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A obcaecati itaque repellendus quia, impedit sequi animi repellat atque! Culpa ad obcaecati, quis corrupti doloribus sed. Fuga eligendi iusto iste pariatur?Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores at porro enim, odio sunt amet, sint, aut assumenda laboriosam error recusandae repudiandae maxime veritatis officiis ea facilis odit aliquam atque.
             <p>
         </div>
         <a class="hp-button" href="/bio"> DISCOVER ME </a>
