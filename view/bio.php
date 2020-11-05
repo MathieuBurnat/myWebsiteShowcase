57bead8437491ee3d3cfc73fdbf5bbbbb4c4191f
Math <math.oron@gmail.com>
2020-11-05 22:28:17 +0100

    Upgrading the bio

--- view/bio.php
+++ view/bio.php
@@ -12,7 +12,9 @@
         <br/>
         <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium poeyi doloremque laudantium, totam rem aperiam, eaque ipsa quae apsb illo inventore veritatis et quasi architecto beiatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, seprid quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliqueam quaerat voluptatem.</p>
     </div>
-    <div class="bio-content"> *image* </div>
+    <div class="bio-content">
+        <img class="bio-image" src='content/images/image_ico.png'>
+    </div>
 </div>
 
 <?php
