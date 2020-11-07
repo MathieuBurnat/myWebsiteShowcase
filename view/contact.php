df580a01ffd2f8986c6fa60b8df9486b1e5a9ef2
Math <math.oron@gmail.com>
2020-11-07 23:46:23 +0100

    Update the contact page
    - Update calendar's styles
    - Update login button's styles
    - Adding text area.

--- view/contact.php
+++ view/contact.php
@@ -1,10 +1,23 @@
 <?php
     require('view/header.php');
 ?>
-<h1> Welcome to contact. </h1>
 
-<?php require("view/calendar.php"); ?>
+<h2>Welcome Master !</h2>
 
+<div class="contact-container default-container">
+    
+    <div class="contact-content">
+        <div class="container-calendar"> <?php require("view/calendar.php"); ?> </div>
+    </div>
 
-<button onclick="document.getElementById('login').style.display='block'" style="width:auto;">Se connecter</button>
+    <div class="contact-content">
+        <textarea id="contact-area" name="contact-area" rows="4" cols="50"> Because i can't.... play factorio...</textarea>
+    </div>
+</div>
+
+<button onclick="document.getElementById('login').style.display='block'" class="contact-button">Se connecter</button>
 <?php require("view/login.php"); ?>
+
+<?php
+    require('view/footer.php');
+?>
