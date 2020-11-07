e6a6f6265ca41cc4d051bcb1b8b3b1c0c1104e3c
Math <math.oron@gmail.com>
2020-11-07 15:51:54 +0100

    Create avar with citation

--- view/info-crea-tech.php
+++ view/info-crea-tech.php
@@ -4,12 +4,16 @@
 
 <div class="info-crea-container default-container">
     <div class="info-crea-content info-crea-left-content">
+        <img src="content/images/img_avatar.png" alt="Avatar" id="info-img-avatar">
         <h3>Name Surname</h3>
         <div class="citation">"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</div>
     </div>
     <div class="info-crea-content">
         <h1>Informatique création technique</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco poriti laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in uienply voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat norin proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco poriti laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in uienply voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat norin proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe repudiandae, id cupiditate quo nemo sint voluptas minus at voluptatum quisquam dignissimos voluptates fuga commodi aliquam excepturi sit error, ullam eos!
+        </p>
         <div class="citation info-citation">"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."" </div>
     </div>
 </div>
