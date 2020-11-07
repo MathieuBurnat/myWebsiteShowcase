269121cbfd2d1175ade4071418f4b148a6a8a66b
Math <math.oron@gmail.com>
2020-11-07 21:58:19 +0100

    update dirties

--- view/contact.php
+++ view/contact.php
@@ -3,10 +3,6 @@
 ?>
 <h1> Welcome to contact. </h1>
 
-
-</head>
-<body>
-
 <h2>Modal Login Form</h2>
 
 <button onclick="document.getElementById('login').style.display='block'" style="width:auto;">Se connecter</button>
