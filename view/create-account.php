487fe4345efcac0c690180d3e7c5b1cdf5f5f778
Math <math.oron@gmail.com>
2020-11-08 22:50:07 +0100

    Check if password are not the same

--- view/create-account.php
+++ view/create-account.php
@@ -1,19 +1,19 @@
 <?php
     require('view/header.php');
 ?>
+<script src="view/create-account.js" defer></script>
 
 <form action="/action_page.php" class="register-container">
     <h1> Créer un compte ! </h1>
 
     <label for="name"><b>Nom</b></label>
-    <input type="text" placeholder="Enter votre nom" name="name" id="name" required>
+    <input type="text" placeholder="Enter votre nom" name="name" id="name" onchange="test()" required>
 
     <label for="psw"><b>Mot de passe</b></label>
-    <input type="password" placeholder="Entrer votre mot de passe" name="psw" id="psw" required>
+    <input type="password"  placeholder="Entrer votre mot de passe" name="psw" id="psw" required>
 
     <label for="psw-repeat"><b>Répéter le mot de passe</b></label>
-    <input type="password" placeholder="Répéter votre mot de passe" name="psw-repeat" id="psw-repeat" required>
-
-    <button type="submit" class="registerbtn">Enregistrer</button>
-    </div>
+    <input type="password" placeholder="Répéter votre mot de passe" name="psw-repeat" id="psw-repeat" onchange="verifyPassword()" required>
+    <p id="error" class="error"></p>
+    <button type="submit" id="submitButton" class="registerbtn">Enregistrer</button>
 </form>
