f052b3f17117e1a5c0b82e775ed6f4349e8fbe3f
Math <math.oron@gmail.com>
2020-11-08 15:09:45 +0100

    Rework on the vertical menu

--- view/contact.php
+++ view/contact.php
@@ -18,16 +18,8 @@
 <?php if($ctrl->displayConnectedButton){ ?>
 <button onclick="document.getElementById('login').style.display='block'" class="contact-connection-button">Se connecter</button>
 <?php }else{ ?>
-<ul>
-    <li>
-        <div class="contact-hey-user-box-old">Welcome <?= $_SESSION["user"];?>!</div>
-    </li>
-    <li>
-        <a class="contact-disconcect-box-old" href="https://www.w3schools.com">Disconnect</a>
-    </li>
-</ul>
 <div class="vertical-menu">
-  <a href="#" class="active contact-hey-user-box">Welcome <?= $_SESSION["user"];?>!</a>
+  <a href="" class="contact-hey-user-box">Welcome <?= $_SESSION["user"];?>!</a>
   <a href="#" class="contact-disconcect-box">Disconnect</a>
 </div>
 <?php } ?>
