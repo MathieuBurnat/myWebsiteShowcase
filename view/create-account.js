c76e83dd4f2d31bd7674b2907f6dcc3e9db2ba12
Math <math.oron@gmail.com>
2020-11-08 23:07:26 +0100

    Disable the button when passwords are incorrect

--- view/create-account.js
+++ view/create-account.js
@@ -16,13 +16,14 @@ function verifyPassword(){
     {
         document.getElementById("psw").classList.add('password-invalid');
         document.getElementById("psw-repeat").classList.add('password-invalid');
+        document.getElementById("submitButton").classList.add('registerbtnDisabled');
         document.getElementById("error").innerHTML = "les mots de passe ne correspondent pas";
         document.getElementById('submitButton').disabled = true;
     }else{
         document.getElementById("psw").classList.remove('password-invalid');
         document.getElementById("psw-repeat").classList.remove('password-invalid');
+        document.getElementById("submitButton").classList.remove('registerbtnDisabled');
         document.getElementById("error").innerHTML = "";
         document.getElementById('submitButton').disabled = false;
     }
-
 }
