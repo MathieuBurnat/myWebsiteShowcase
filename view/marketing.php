92f0786f0b1012013e37a60f2b6a85d0d06d5585
Math <math.oron@gmail.com>
2020-11-15 13:25:03 +0100

    Adding thara's picture with it name

--- view/marketing.php
+++ view/marketing.php
@@ -14,8 +14,8 @@
         <div class="citation market-citation">"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."" </div>
     </div>
     <div class="market-content market-left-content">
-        <img src="content/images/img_avatar.png" alt="Avatar" id="info-img-avatar">
-        <h3>Name Surname</h3>
+        <img src="content/images/thara.jpeg" alt="Avatar" id="info-img-avatar">
+        <h3>Thara Scippa</h3>
         <div class="citation">"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</div>
     </div>
 </div>
